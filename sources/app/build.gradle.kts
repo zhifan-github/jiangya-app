e3adae6caca4951fbfc2fc5c36abed3b63162b65
zhifan <614963420@qq.com>
2026-07-16 23:50:15 +0800

    chore: prepare v0.1.0-beta.1

--- sources/app/build.gradle.kts
+++ sources/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-beta.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
